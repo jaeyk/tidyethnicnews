db6ff32e42b866a21ef9ffec442e9dcabbf7ce20
Jae Yeon Kim <jaeyeonkim@berkeley.edu>
2020-09-10 20:04:35 -0700

    from pmap to map

--- R/html_to_dataframe_all.R
+++ R/html_to_dataframe_all.R
@@ -3,25 +3,27 @@
 #' @param dir_path A directory path where a user saved the HTML files containing the search results from the Ethnic NewsWatch database. This input should be a string vector. 
 #' @return A dataframe with four columns ("text", "source", "author", "date")
 #' @importFrom dplyr full_join
-#' @importFrom purrr pmap
+#' @importFrom purrr map
 #' @importFrom purrr reduce
 #' @export
 
 html_to_dataframe_all <- function(dir_path){
   
-# Load all HTML files in the designated file path 
-filename <- list.files(dir_path, 
-                       pattern = '*.html', 
-                       full.names = TRUE)
-
-df <- list(filename) %>%
+  dir_path <- "/home/jae/muslim_newspapers-selected/full_version/"
+  
+  # Load all HTML files in the designated file path 
+  filename <- list.files(dir_path, 
+                         pattern = '*.html', 
+                         full.names = TRUE)
+  
+  df <- filename %>%
     
     # Apply html_to_dataframe function to items on the list 
-    pmap(~html_to_dataframe(.)) %>%
+    map(~html_to_dataframe(.)) %>%
     # Full join the list of dataframes 
     reduce(full_join, by = c("text", "source", "author","date"))
-
-# Output
-df 
-
+  
+  # Output
+  df 
+  
 }
